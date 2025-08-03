fded623d8bda338bd15d7bed52f09371e5372536
Priyanka <95175243+Priyanka-18-stack@users.noreply.github.com>
2025-08-03 14:07:46 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
   }
   function send(){
     setstatus(true)
-    axios.post("http://localhost:5000/sendemail",{msg:msg,emailList:emailList})
+    axios.post("https://bulkmail1-r9mt.onrender.com/sendemail",{msg:msg,emailList:emailList})
     .then(function(data)
     {
       if(data.data===true){
