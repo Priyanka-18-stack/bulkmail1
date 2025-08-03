c652c8f7509e4c410f54b938dbae0be61cdbee64
Priyanka-18-stack <priyabef@gmail.com>
2025-08-03 13:05:40 +0530

    Initial commit for fullstack bulkmail app

---
+++ backend/index.js
@@ -0,0 +1,67 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+
+const nodemailer = require("nodemailer")
+const mongoose=require("mongoose")
+app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb+srv://priyabef:123@cluster0.nk5u2ws.mongodb.net/passkey?retryWrites=true&w=majority&appName=Cluster0").then(function(){
+  console.log("connected to db")
+}).catch(function(){console.log("failed to connect")})
+
+const credential=mongoose.model("credential",{},"bulkmail")
+
+// Create a test account or replace with real credentials.
+
+ app.post("/sendemail", function (req, res) {
+  const msg = req.body.msg;
+  const emailList = req.body.emailList;
+  credential.find().then(function(data){
+  const transporter = nodemailer.createTransport({
+  service:"gmail",
+  auth: {
+    user:data[0].toJSON().user,
+    pass:data[0].toJSON().pass,
+  },
+});
+  new Promise(async function(resolve,reject){
+    try{
+     for (let i = 0; i < emailList.length; i++) {
+      await transporter.sendMail(
+      {
+        from: "priyabef@gmail.com",
+        to: emailList[i],
+        subject: "message from bulk mail app",
+        text: msg
+      },
+      )
+      console.log("Email sent to:"+emailList[i])
+   }
+  
+  resolve("success")
+}
+  catch(error){
+    reject("failed")
+  }
+  }).then (function(){
+    res.send(true)
+  }).catch(function(){
+    res.send(false)
+  })
+
+}).catch(function(error){
+  console.log(error)
+})
+
+
+
+
+  
+ 
+});
+
+
+app.listen(5000,()=>{
+  console.log("server is running..")
+})
